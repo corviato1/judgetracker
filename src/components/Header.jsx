42c515b9f7f09bcc365caed4f782ae455406a856
JudgeTracker <judgetracker@replit.com>
2026-04-14 09:38:25 +0000

    Task #29: Make header logo link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
@@ -14,7 +15,15 @@ const Header = () => {
         }}
       >
         <div>
-          <h1 style={{ margin: 0, fontSize: "1.5rem" }}>JudgeTracker.info</h1>
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
+            <h1 style={{ margin: 0, fontSize: "1.5rem" }}>JudgeTracker.info</h1>
+          </Link>
           <p style={{ margin: "0.25rem 0 0", fontSize: "0.9rem", opacity: 0.9 }}>
             Transparent insight into judicial records, rulings, and case
             histories.
